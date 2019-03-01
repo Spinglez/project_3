013d7e53513ce2d7ff2df1bf11831e4d80460ab9
tam <taam.tr17@gmail.com>
2019-02-28 17:38:41 -0800

    Testing Sass Variables

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
