c132ca4bd6ad869ecdff91e02eeaeeaffdd0920a
Spinglez <42229372+Spinglez@users.noreply.github.com>
2019-03-07 14:34:31 -0800

    State is being set on component mounting using an axios call to set state in the database, we can also access it from state now and performed a short circuit for rendering. not will flesh out page and add in components for styling

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,29 +1,52 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import call from '../../utils/call'
+import Ringloader from '../RingLoader/loader'
+
 
 export class Profile extends Component {
 
   state = {
     user: null,
     dBdata : {},
-
+    email: null,
+    update: false
   }
 
   componentDidMount() {
-    call.get(this.state.email)
+    call.get('test@gmail.com')
       .then((res) => {
         this.setState({dBdata: res.data})
+        console.log('this content is set');
+      }).then(()=>{
+        this.setState({update : true})
+        console.log('this has set state as update to true');
+      }).catch(err => {
+        if (err) console.log(err);
       })
+  }
+
+  componentWillUnmount() {
+    this.setState({update: false})
   }
 
   render() {
     return (
       <div>
-        {!this.state.dBdata ?
-        <header><h1>Welcome to your Profile page</h1></header>
-        :
-        <div>Testing</div>
-        }
+        <header>This is a header!</header>
+          {this.state.update === true &&
+            <Fragment>
+              <h1>this is rendered when update is true:</h1>
+              <div>
+                <p>Welcome! {this.state.dBdata.data.firstName}, it's nice to see you today</p>
+              </div>
+            </Fragment>
+          }
+          {this.state.update === false &&
+            <Fragment>
+              <h1>Please wait while we load your profile page, thanks for your patience!</h1>
+              <Ringloader />
+            </Fragment>
+          }
       </div>
     )
   }
--- client/src/utils/call.js
+++ client/src/utils/call.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
-const URL = "http://localhost/3001/api"
+const URL = "http://localhost:3001/api/Users"
 
 export default {
   get : (query) => {
     return axios.get(URL + query)
+  },
+  post : (post) =>{
+    return axios.post(URL, post)
   }
 }
