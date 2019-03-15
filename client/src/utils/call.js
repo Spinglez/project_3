0c49e55a0441237b8a5747ed373224d9632a9a2b
Spinglez <42229372+Spinglez@users.noreply.github.com>
2019-03-14 20:33:41 -0700

    minor for saving some files and dependancies

--- client/src/utils/call.js
+++ client/src/utils/call.js
@@ -1,13 +1,8 @@
 import axios from 'axios';
 
-<<<<<<< HEAD
 const getURL = "/api/Users"
+const getSavedMovies = "/api/savedmovies"
 const matchURL = "/api/match"
-=======
-const getURL = "http://localhost:3001/api/Users"
-const getSavedMovies = "http://localhost:3001/api/savedmovies"
-const matchURL = "http://localhost:3001/api/match"
->>>>>>> 560159605a1bd72730738f84f1ae91cf072b4bcc
 
 export default {
   get : (query) => {
