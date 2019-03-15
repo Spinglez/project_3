6b85e974efc031641d9b9a92184ac9990b7d69a6
nVar <nkeledjian@gmail.com>
2019-03-14 20:00:02 -0700

    removed Fragment from SavedMovies component - keys for movie data is set

--- client/src/components/SavedMovies/SavedMovies.js
+++ client/src/components/SavedMovies/SavedMovies.js
@@ -1,19 +1,17 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 
 const SavedMovies = (props) => {
     let moviesData = props.data;
     console.log('movieData', moviesData)
       return (
           moviesData.data.map(movie =>
-          <Fragment>
-          <div key={movie._id}>  
-            {/* <p>{movie.movieTitle}</p> */}
+            <div key={movie._id}>  
+              {/* <p>{movie.movieTitle}</p> */}
               <img
               src={movie.moviePoster}
               alt={movie.movieTitle}
               />
-            </div>     
-          </Fragment>
+            </div>
           )
       )
 }
