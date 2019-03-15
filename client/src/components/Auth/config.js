25ca542715a7b563435b0a4413d7559091f9eee4
Spinglez <42229372+Spinglez@users.noreply.github.com>
2019-03-14 19:11:10 -0700

    changed  auth config, pushing up build

--- client/src/components/Auth/config.js
+++ client/src/components/Auth/config.js
@@ -2,7 +2,7 @@ export const config = {
   clientId: "UQupq0BPYkKOD2rVYC30puOL0Vu1GLtq",
   domain: "movienite.auth0.com",
   // For production, redirect and logoutUrl should be on port 3005
-  redirect: "http://localhost:3000/callback",
+  redirect: "https://matchmovienite.herokuapp.com/callback",
   audience: "https://movienite.auth0.com/userinfo",
-  logoutUrl: "http://localhost:3000"
+  logoutUrl: "https://matchmovienite.herokuapp.com"
 };
