54a8c34426f14b6dae35ab272188ae9970bfbc07
tam <taam.tr17@gmail.com>
2019-03-15 23:49:09 -0700

    Changed fonts, card-size, color buttons. Finished

--- client/src/components/MovieCard/MovieCard.js
+++ client/src/components/MovieCard/MovieCard.js
@@ -7,17 +7,30 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
 import Tooltip from '@material-ui/core/Tooltip';
+import styled from 'styled-components';
 
 const styles = {
   card: {
-    maxWidth: 345
+    maxWidth: 345,
+    marginTop: 8,
+    marginLeft: 8
   },
   media: {
     height: 140,
   },
 };
+
+const TitleStyle = styled.div`
+    color: #002744;
+    font-size: 1.8rem;
+    font-family: 'Oswald', sans-serif;
+`;
+
+const OverviewStyle = styled.p`
+      font-family: 'Lato', sans-serif;
+      color: #01579b;
+`;
 
 class MovieCard extends Component{
    state = {
@@ -34,6 +47,7 @@ class MovieCard extends Component{
 
   return (
     <Card className={classes.card}>
+
       <CardActionArea>
         <CardMedia
           key={key}
@@ -42,28 +56,33 @@ class MovieCard extends Component{
         //   ${ + this.props.image}
           title="Contemplative Reptile"
         />
-        <CardContent key={key}>
-          <Typography gutterBottom variant="h5" component="h2" style={{textAlign:"center"}}>
+        <CardContent key={key} style={{height: "225px"}}>
+          <TitleStyle gutterBottom variant="h5" component="h2" style={{textAlign:"center"}}>
             {title}
-          </Typography>
-          <Typography component="p" style={{overflowY:"scroll", maxHeight: "100px"}}>
+          </TitleStyle>
+          <OverviewStyle component="p" style={{overflowY:"scroll", maxHeight: "100px"}}>
             {overview}
-          </Typography>
+          </OverviewStyle>
         </CardContent>
       </CardActionArea>
+
       <CardActions style={{display:"grid"}}>
         <Button size="small" color="primary" disabled={this.state.disabled} onClick={()=> this.disableButton(title, overview, image, voteScore)}>
           Save
         </Button>
-        <Button size="small" color="primary" onClick={()=> removeMovie(id)}>
+
+        <Button size="small" color="secondary" onClick={()=> removeMovie(id)}>
           Clear
         </Button>
+
         <Tooltip title={`Popularity: ${voteScore}/10`} placement="bottom">
-        <Button size="small" color="primary">
-          Popularity
-        </Button>
+          <Button size="small" color="primary">
+            Popularity
+          </Button>
         </Tooltip>
+
       </CardActions>
+
     </Card>
   );
   }
